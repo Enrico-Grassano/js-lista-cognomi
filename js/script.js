a561fa23286a1de5b4019cc5c761bbc973eb3836
Enrico-Grassano <grassano.enrico1996@gmail.com>
2021-04-30 15:23:14 +0200

    Prompt and array created

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//1. chiedi all'utente il cognome.
+var cognomeUtente = prompt("Qual'è il tuo cognome?");
+console.log(cognomeUtente);
+//2. inseriscilo in un array con altri cognomi: 'Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'.
+var cognomiLista = ["Bianchi", "Rossi", "Verdi", "Gialli", "Neri"];
+cognomiLista.push(cognomeUtente);
+console.log(cognomiLista);
+//3. stampa la lista ordinata alfabeticamente.
+
+
+//4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova.
